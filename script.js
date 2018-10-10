a0d398bb3989054291d490ca2d5408615b4263c0
CarmenoI <43419707+CarmenoI@users.noreply.github.com>
2018-10-10 01:29:00 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="UTF-8">
+
+	<title>Parchment, Stone, or Blade?</title>
+
+	<link rel="stylesheet" href="style/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Bangers|Comfortaa" rel="stylesheet">
+
+</head>
+
+	<body>
+
+		<h1>Rock, Paper, Scissors, Lizard, Spock Death Match</h1>
+
+		<p>The Grim Reaper has challenged you for your soul<strong> Rock</strong>, <strong>Paper</strong> , <strong>Scissors</strong>, <strong>Lizard</strong>,or <strong>Spock</strong> choose wisely, this is LIFE VS DEATH.</p>
+
+		<div class="scoreboard">
+		  <h2>Score</h2>
+		  <table>
+		    <tbody>
+		      <tr>
+		        <td id="humanScore">0</td>
+		        <td id="computerScore">0</td>
+		      </tr>
+		    </tbody>
+		    <tfoot>
+		      <tr>
+		        <th>Player</th>
+		        <th>Grim</th>
+		      </tr>
+		    </tfoot>
+		  </table>
+		</div>
+
+		<h2>Choose Wisely Mortal</h2>
+
+		<button id="rock">Rock</button>
+
+		<button id="paper">Paper</button>
+
+		<button id="scissors">Scissors</button>
+
+		<button id="spock">Spock</button>
+
+		<button id="lizard">Lizard</button>
+
+		<div id="status"></div>
+		<script src="javascipt/main.js"> </script>
+	</body>
+
+</html>
